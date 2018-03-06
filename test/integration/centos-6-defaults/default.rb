b3f075cb16ebf0e555bc94924b8c6b53caced72a
Ricardo Oliveira <ricardolotr@gmail.com>
2018-03-06 16:49:09 +0000

    refactoring password quality to use authconfig on redhat 7 (#8)

--- test/integration/centos-6-defaults/default.rb
+++ test/integration/centos-6-defaults/default.rb
@@ -1,7 +1,7 @@
 describe file('/etc/pam.d/password-auth') do
-  its('content') { should include('password    requisite     pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1') }
+  its('content') { should include('password    requisite     pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 minclass=3 maxrepeat=0 maxclassrepeat=0') }
 end
 
 describe file('/etc/pam.d/system-auth') do
-    its('content') { should include('password    required      pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1') }
+    its('content') { should include('password    required      pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 minclass=3 maxrepeat=0 maxclassrepeat=0') }
   end
--- test/integration/centos-7-defaults/default.rb
+++ test/integration/centos-7-defaults/default.rb
@@ -4,4 +4,7 @@ describe parse_config_file('/etc/security/pwquality.conf') do
   its('ucredit') { should eq '-1' }
   its('ocredit') { should eq '-1' }
   its('lcredit') { should eq '-1' }
+  its('minclass') { should eq '3' }
+  its('maxrepeat') { should eq '0' }
+  its('maxclassrepeat') { should eq '0' }
 end
